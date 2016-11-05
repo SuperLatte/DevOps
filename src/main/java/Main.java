52336b5b5beac6c07d898589a9aee745318a8741
Yida Zhou <arkie01@163.com>
2016-11-05 20:38:26 +0800

    initialize proj

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by super on 2016/11/5.
- */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("DevOps has been started.");
-    }
-}
---
+++ src/main/java/main/Application.java
@@ -0,0 +1,32 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by super on 2016/11/5.
+ */
+@RestController
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
