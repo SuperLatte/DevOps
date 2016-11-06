fe0e7cafcd98056dc8a756d1c519dbdfcb9b7cf6
Sysan <shifanglei7@Gmail.com>
2016-11-06 12:39:41 +0800

    add controller

---
+++ src/main/java/controller/ProjectController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/project")
+@EnableAutoConfiguration
+public class ProjectController {
+
+}
