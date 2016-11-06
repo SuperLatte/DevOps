88b897996f34d0459592bb9e178f78792934bb72
lujxu <329088775@qq.com>
2016-11-06 15:54:02 +0800

    user login

--- src/main/java/com/devops/controller/UserController.java
+++ src/main/java/com/devops/controller/UserController.java
@@ -5,15 +5,24 @@ package com.devops.controller;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author Shifang
- *
- */
+import com.devops.dto.UserDTO;
+
 @RestController
-@RequestMapping("/user")
 @EnableAutoConfiguration
 public class UserController {
 
+	@RequestMapping(value="/login",method=RequestMethod.POST)
+	public @ResponseBody UserDTO login(@RequestParam(value="username") String username,@RequestParam(value="password") String password){
+		UserDTO user=new UserDTO();
+		user.setUsername(username);
+		user.setUid("123");
+		return user;
+	}
+	
+	
 }
---
+++ src/main/java/com/devops/dto/UserDTO.java
@@ -0,0 +1,30 @@
+package com.devops.dto;
+
+public class UserDTO {
+	
+	private String uid;
+	/** the name of user **/
+	private String name;
+	/** login name **/
+	private String username;
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+}
