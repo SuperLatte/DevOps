7856520cd9d9dfe754c89ac7fc17083ce836b263
Yida Zhou <arkie01@163.com>
2016-11-09 17:12:07 +0800

    fix smells

--- src/main/java/com/devops/utils/JDBCConfig.java
+++ src/main/java/com/devops/utils/JDBCConfig.java
@@ -1,8 +1,6 @@
 package com.devops.utils;
 
-import java.io.FileInputStream;
 import java.sql.*;
-import java.util.Properties;
 
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
@@ -28,30 +26,30 @@ public class JDBCConfig {
     @Value( "${rport}")
     private int rport;//远程MySQL服务端口
     @Value( "${ssh_user}")
-    private String ssh_user;//SSH连接用户名
+    private String sshUser;//SSH连接用户名
     @Value( "${ssh_password}")
-    private String ssh_password;//SSH连接密码
+    private String sshPassword;//SSH连接密码
     @Value( "${ssh_host}")
-    private String ssh_host;//SSH服务器
+    private String sshHost;//SSH服务器
     @Value( "${ssh_port}")
-    private int ssh_port;//SSH访问端口
+    private int sshPort;//SSH访问端口
     @Value( "${db_user}")
-    private String db_user;//SSH连接用户名
+    private String dbUser;//SSH连接用户名
     @Value( "${db_password}")
-    private String db_password;//SSH连接密码
+    private String dbPassword;//SSH连接密码
 
     @Bean
     public Connection connection() throws JSchException, SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
         JSch jsch = new JSch();
-        Session session = jsch.getSession(ssh_user, ssh_host, ssh_port);
-        session.setPassword(ssh_password);
+        Session session = jsch.getSession(sshUser, sshHost, sshPort);
+        session.setPassword(sshPassword);
         session.setConfig("StrictHostKeyChecking", "no");
         session.connect();
         logger.info(session.getServerVersion());//这里打印SSH服务器版本信息
         int assingedPort = session.setPortForwardingL(lport, rhost, rport);
         logger.info("localhost:" + assingedPort + " -> " + rhost + ":" + rport);
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:"+lport+"/risk", db_user, db_password);
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:"+lport+"/risk", dbUser, dbPassword);
         logger.info("MySQL has been connected!");
         return connection;
     }
