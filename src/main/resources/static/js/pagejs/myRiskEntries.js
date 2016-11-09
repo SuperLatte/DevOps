05385a943e682890ba1637ea0e58798221ed502a
princessV <puddingtea07@hotmail.com>
2016-11-10 00:46:05 +0800

    update page and js

--- src/main/resources/static/js/pagejs/myRiskEntries.js
+++ src/main/resources/static/js/pagejs/myRiskEntries.js
@@ -3,11 +3,20 @@
  */
 storage = window.localStorage;
 
-function dataRender(){
+function dataRender() {
     $('h2[data-aria="username"]').text(storage.getItem("username"));
     $('a[data-aria="username"]').text(storage.getItem("username"));
 }
 
+
+
 $(document).ready(function () {
    dataRender();
+
+    $('a[role="viewDetails"]').click(function () {
+        let rid = $(this).parent('td').attr('rid');
+        $.post('./viewDetails', {rid: rid}, function (data) {
+            window.location.href = data.url;
+        })
+    })
 });
---
+++ src/main/resources/static/js/pagejs/riskDetail.js
@@ -0,0 +1,8 @@
+/**
+ * Created by puddingtea07 on 11/10/16.
+ */
+$(document).ready(function () {
+    $('input[type="checkbox"]').iCheck({
+        checkboxClass: 'icheckbox_flat-green'
+    });
+});
